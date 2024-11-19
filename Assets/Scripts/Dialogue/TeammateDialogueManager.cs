54ad9abb16be05de26d4a1aaa4771366227165e5
Sbin <135858127+zzangsbin97@users.noreply.github.com>
2024-11-20 03:01:55 +0900

    241120
    Teammate 객체와 상호작용 했을 시 TeammateDialogue에 "말하는 동료의 이름은 (teammateName)" 추가

--- Assets/Scripts/Dialogue/TeammateDialogueManager.cs
+++ Assets/Scripts/Dialogue/TeammateDialogueManager.cs
@@ -1,18 +1,25 @@
 using System.Collections;
 using System.Collections.Generic;
-using UnityEngine;
 using UnityEngine.UI;
+using UnityEngine;
+using TMPro;
 
 public class TeammateDialogueManager : MonoBehaviour {
-    
-    public Text talkScript;
-    public GameObject talkingTeammate;
+	public TMP_Text talkText; // Inspector에서 할당
+	public GameObject talkingTeammate;
 
-    public void Talk(GameObject talkingTeammate) {
-        this.talkingTeammate = talkingTeammate;
-        talkScript.text = "말하는 동료의 이름은 " + talkingTeammate.name;
-    }
+	public void Talk(GameObject talkingTeammate, string teammateName) {
+		if (talkText == null) {
+			Debug.LogError("talkText가 Inspector에서 할당되지 않았습니다!");
+			return;
+		}
 
+		if (talkingTeammate == null) {
+			Debug.LogError("talkingTeammate가 null입니다! 유효한 GameObject를 전달하세요.");
+			return;
+		}
 
-
+		this.talkingTeammate = talkingTeammate;
+		talkText.text = "말하는 동료의 이름은 " + teammateName;
+	}
 }
