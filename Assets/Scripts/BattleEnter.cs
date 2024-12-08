d33337a52f1fd928882b0758bee27e76d746f4b4
Sbin <135858127+zzangsbin97@users.noreply.github.com>
2024-12-08 17:12:30 +0900

    241208
    Boss의 경우 자동으로 대화 진행하고 Battle씬으로 들어가게 수정(구현)

--- Assets/Scripts/BattleEnter.cs
+++ Assets/Scripts/BattleEnter.cs
@@ -18,7 +18,6 @@ public class BattleEnter : MonoBehaviour {
 
 	private void OnTriggerEnter2D(Collider2D other) {
 
-
 		// Monster 레이어와 충돌 시 처리
 		if (other.gameObject.TryGetComponent<Monster>(out Monster collidedMonster)) {
 			// MonsterManager에 currentMonster 설정
@@ -27,10 +26,12 @@ public class BattleEnter : MonoBehaviour {
 			Debug.Log($"충돌한 몬스터: {collidedMonster.MonsterName}, 공격력: {collidedMonster.attackPower}");
 
 			if (Math.Abs(collidedMonster.monsterCategory) != 1) {
+				// 스크립트가 있으니까 대화 진행
 				TeammateDialogueManager.Instance.ProgressDialogue(collidedMonster.gameObject); // GameObject 전달
+			} else {
+				// 잡몹은 바로 Battle 씬으로 전환
+				SceneManager.LoadScene("Battle");
 			}
-
-			SceneManager.LoadScene("Battle"); // Battle 씬으로 전환
 		}
 
 		/*
@@ -38,9 +39,9 @@ public class BattleEnter : MonoBehaviour {
 		if (other.gameObject.layer == LayerMask.NameToLayer("FieldMonster_b")) {
 			Monster collidedMonster = other.gameObject.GetComponent<Monster>();
 			if (collidedMonster != null) {
-				MonsterManager.Instance.currentMonster = collidedMonster; // 싱글톤을 통해 설정
+				MonsterManager.Instance.currentMonster = collidedMonster; 
 				Debug.Log($"충돌한 몬스터: {collidedMonster.MonsterName}, 공격력: {collidedMonster.attackPower}");
-				SceneManager.LoadScene("Battle"); // Battle 씬으로 전환
+				SceneManager.LoadScene("Battle"); 
 			}
 		}
 
@@ -48,9 +49,9 @@ public class BattleEnter : MonoBehaviour {
 		if (other.gameObject.layer == LayerMask.NameToLayer("FieldMonster_a")) {
 			Monster collidedMonster = other.gameObject.GetComponent<Monster>();
 			if (collidedMonster != null) {
-				MonsterManager.Instance.currentMonster = collidedMonster; // 싱글톤을 통해 설정
+				MonsterManager.Instance.currentMonster = collidedMonster; 
 				Debug.Log($"충돌한 몬스터: {collidedMonster.MonsterName}, 공격력: {collidedMonster.attackPower}");
-				SceneManager.LoadScene("Battle"); // Battle 씬으로 전환
+				SceneManager.LoadScene("Battle"); 
 			}
 		}
 
@@ -58,9 +59,9 @@ public class BattleEnter : MonoBehaviour {
 		if (other.gameObject.layer == LayerMask.NameToLayer("Boss")) {
 			Monster collidedBoss = other.gameObject.GetComponent<Monster>();
 			if (collidedBoss != null) {
-				MonsterManager.Instance.currentMonster = collidedBoss; // 싱글톤을 통해 설정
+				MonsterManager.Instance.currentMonster = collidedBoss; 
 				Debug.Log($"보스와 충돌: {collidedBoss.MonsterName}, 공격력: {collidedBoss.attackPower}");
-				SceneManager.LoadScene("Battle"); // Battle 씬으로 전환
+				SceneManager.LoadScene("Battle"); 
 			}
 		}
 		*/
--- Assets/Scripts/Dialogue/TeammateDialogueManager.cs
+++ Assets/Scripts/Dialogue/TeammateDialogueManager.cs
@@ -2,31 +2,33 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using TMPro;
+using UnityEngine.SceneManagement;
 
 public class TeammateDialogueManager : MonoBehaviour {
-
 	public static TeammateDialogueManager Instance;
 
-	public GameObject dialoguePanel; // 대화 패널
-	public TMP_Text talkText; // 대화 텍스트 UI
-	public DialogueText dialogueText; // 대사 데이터
-	public GameObject talkingObject; // 현재 대화 중인 팀원
-	public int textIndex = 0; // 대사 인덱스
+	public GameObject dialoguePanel;
+	public TMP_Text talkText;
+	public DialogueText dialogueText;
+	public GameObject talkingObject;
+	public int textIndex = 0;
 	private bool isTalking = false;
 
-	// public TeammateManager tempTeammateManager;
-
+	private void Awake() {
+		if (Instance == null) {
+			Instance = this;
+			DontDestroyOnLoad(gameObject);
+		} else {
+			Destroy(gameObject);
+		}
+	}
 
 	public void ProgressDialogue(GameObject TalkingObject) {
-
 		if (this.talkingObject != TalkingObject) {
-			// 새로운 팀원과 대화 시작
 			this.talkingObject = TalkingObject;
-			// Teammate tempTeammate = talkingTeammate.GetComponent<Teammate>();
-			// tempTeammateManager.teammates.Add(tempTeammate);
-			textIndex = 0; // 인덱스 초기화
-			dialoguePanel.SetActive(true); // 대화 패널 활성화
-
+			textIndex = 0;
+			dialoguePanel.SetActive(true);
+			isTalking = true;
 		}
 
 		Id teammateId = TalkingObject.GetComponent<Id>();
@@ -38,33 +40,34 @@ public class TeammateDialogueManager : MonoBehaviour {
 		ShowDialogue(teammateId.objectId);
 	}
 
+	private void Update() {
+		if (isTalking && Input.GetKeyDown(KeyCode.E)) {
+			if (talkingObject != null) {
+				Id teammateId = talkingObject.GetComponent<Id>();
+				ShowDialogue(teammateId.objectId);
+			}
+		}
+	}
+
 	public void ShowDialogue(int id) {
 		string dialogueTemp = dialogueText.GetText(id, textIndex);
 
 		if (dialogueTemp == null) {
-			// 대화 종료
 			EndDialogue();
 			return;
 		}
 
-		// 다음 대사 출력
 		talkText.text = dialogueTemp;
 		textIndex++;
 	}
 
 	void EndDialogue() {
-		dialoguePanel.SetActive(false); // 패널 비활성화
-		textIndex = 0; // 인덱스 초기화
-		talkingObject.SetActive(false); // 상호작용 대상 비활성화
-		talkingObject = null; // 상태 초기화
-	}
+		dialoguePanel.SetActive(false);
+		textIndex = 0;
+		talkingObject.SetActive(false);
+		talkingObject = null;
+		isTalking = false;
 
-	public void Awake() {
-		if (Instance == null) {
-			Instance = this;
-			DontDestroyOnLoad(gameObject); // 씬 전환 시 파괴되지 않음
-		} else {
-			Destroy(gameObject); // 중복된 인스턴스 방지
-		}
+		SceneManager.LoadScene("Battle");
 	}
 }
